35a67b46cf78eaa39d79e98c9e183ff1258eefa5
Guilherme <guilhermes_neves@hotmail.com>
2022-03-15 11:48:17 -0300

    desafio da página de login

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+// Cenários:
+
+// 1 - Login com sucesso
+// 2 - Senha incorreta
+// 3 - Email no formato inválido
+// 4 - Campos obrigatórios (dois ITs)
+
+
+// Dicas 
+
+// 1 - Para fazer login, é necessário já ter uma conta cadastrada
+// 2 - Usar contextos
+// 3 - Para fazer o login com sucesso, vc deverá componentizar alguma coisa
