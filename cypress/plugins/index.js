1bb1e4a4e46a90635ce53febff3c57f31f9eaf06
Guilherme <guilhermes_neves@hotmail.com>
2022-03-15 11:36:47 -0300

    configurações do banco de dados no arquivo cypress.json

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,13 +22,9 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  const pool = new Pool({
-    host: 'jelani.db.elephantsql.com',
-    user: 'uykzcycr',
-    password: '63VIaISoFDJmik4ty5gxQS-wlmsXZxRF',
-    database: 'uykzcycr',
-    port: 5432
-  })
+  const configJson = require(config.configFile)
+
+  const pool = new Pool(configJson.dbConfig)
 
   on('task', {
     removeUser(email) {
